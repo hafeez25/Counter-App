b8ea6bba6b768f54237f4c46e40a1a89bb982926
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-03-27 00:30:36 +0530

    payload add

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-import React, { useState, useReducer } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  let counter = useSelector((state) => state.counter);
+  console.log(counter);
 
-  const increase = () => {
-    setCount(count + 1);
-  };
-
-  const decrease = () => {
-    setCount(count - 1);
-  };
-
-  const handleData = (data) => {
-    console.log(data);
-  };
-  const initialState = { count: 0 };
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case "increment":
-        return { count: state.count + 1 };
-      case "decrement":
-        return { count: state.count - 1 };
-      default:
-        throw new Error();
-    }
-  }
-  const [state, dispatch] = useReducer(reducer, initialState);
-
+  // const counter = 5;
+  const dispatch = useDispatch();
   return (
     <div className="container">
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { legacy_createStore as createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "DECREMENT") {
+    return { counter: state.counter - 1 };
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
